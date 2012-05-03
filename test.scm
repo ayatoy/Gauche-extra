c70bffb14166d03aaf064e7c0e732b07b761c22d
ayatoy <ayatoy@gmail.com>
2012-05-03 18:31:15 +0900

    Fixed test

--- test.scm
+++ test.scm
@@ -1,7 +1,17 @@
 (use gauche.test)
 
 (test-start "extra")
-(use extra)
-(test-module 'extra)
+
+(use extra.list)
+(test-module 'extra.list)
+
+(use extra.string)
+(test-module 'extra.string)
+
+(use extra.condition)
+(test-module 'extra.condition)
+
+(use extra.dev)
+(test-module 'extra.dev)
 
 (test-end)
